abb23852c05d6a0687f4e3837b9c72c8bcddd7a2
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2022-12-23 22:01:16 +0700

    FAQ View Done

--- DTETI Room Booker/Forms/FormFAQ.Designer.cs
+++ DTETI Room Booker/Forms/FormFAQ.Designer.cs
@@ -29,21 +29,82 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.label1 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.customTextBox1 = new DTETI_Room_Booker.CustomTextBox();
+            this.txtBoxName = new DTETI_Room_Booker.CustomTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.vScrollBar1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1067, 562);
+            this.panel1.Size = new System.Drawing.Size(1073, 1080);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnSend);
+            this.panel3.Controls.Add(this.customTextBox1);
+            this.panel3.Controls.Add(this.txtBoxName);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 384);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1073, 534);
+            this.panel3.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnSend.ForeColor = System.Drawing.Color.White;
+            this.btnSend.Location = new System.Drawing.Point(807, 32);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(216, 482);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.splitContainer1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(50);
+            this.panel2.Size = new System.Drawing.Size(1073, 384);
+            this.panel2.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.DarkBlue;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(714, 284);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // vScrollBar1
             // 
@@ -52,27 +113,94 @@
             this.vScrollBar1.Size = new System.Drawing.Size(80, 1332);
             this.vScrollBar1.TabIndex = 0;
             // 
-            // label1
+            // listView2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(40, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(191, 44);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Questions";
+            this.listView2.BackColor = System.Drawing.Color.DarkBlue;
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView2.ForeColor = System.Drawing.Color.White;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(0, 0);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(255, 284);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // customTextBox1
+            // 
+            this.customTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.customTextBox1.BackColor = System.Drawing.Color.Orange;
+            this.customTextBox1.BorderColor = System.Drawing.Color.Blue;
+            this.customTextBox1.BorderFocusColor = System.Drawing.Color.DarkViolet;
+            this.customTextBox1.BorderSize = 3;
+            this.customTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customTextBox1.ForeColor = System.Drawing.Color.White;
+            this.customTextBox1.Location = new System.Drawing.Point(36, 110);
+            this.customTextBox1.Multiline = true;
+            this.customTextBox1.Name = "customTextBox1";
+            this.customTextBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.customTextBox1.PasswordChar = false;
+            this.customTextBox1.Size = new System.Drawing.Size(729, 404);
+            this.customTextBox1.TabIndex = 1;
+            this.customTextBox1.Texts = "Question";
+            this.customTextBox1.UnderlinedStyle = true;
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxName.BackColor = System.Drawing.Color.Orange;
+            this.txtBoxName.BorderColor = System.Drawing.Color.Blue;
+            this.txtBoxName.BorderFocusColor = System.Drawing.Color.DarkViolet;
+            this.txtBoxName.BorderSize = 0;
+            this.txtBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxName.ForeColor = System.Drawing.Color.White;
+            this.txtBoxName.Location = new System.Drawing.Point(36, 32);
+            this.txtBoxName.Multiline = false;
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Padding = new System.Windows.Forms.Padding(7);
+            this.txtBoxName.PasswordChar = false;
+            this.txtBoxName.Size = new System.Drawing.Size(729, 46);
+            this.txtBoxName.TabIndex = 0;
+            this.txtBoxName.Texts = "Name";
+            this.txtBoxName.UnderlinedStyle = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(50, 50);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(973, 284);
+            this.splitContainer1.SplitterDistance = 255;
+            this.splitContainer1.TabIndex = 0;
             // 
             // FormFAQ
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1073, 1080);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormFAQ";
             this.Text = "FAQ";
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -81,6 +209,13 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.VScrollBar vScrollBar1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private CustomTextBox customTextBox1;
+        private CustomTextBox txtBoxName;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
--- DTETI Room Booker/Forms/FormFAQ.cs
+++ DTETI Room Booker/Forms/FormFAQ.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +18,8 @@ namespace DTETI_Room_Booker.Forms
         {
             InitializeComponent();
             LoadTheme();
+            LoadListQuestion(listView2);
+            LoadListAnswer(listView1);
         }
         public void LoadTheme()
         {
@@ -30,10 +34,100 @@ namespace DTETI_Room_Booker.Forms
                 }
             }
         }
+        public void LoadListQuestion(ListView listView)
+        {
+            string conn = "https://localhost:7045/api/Questions";
+            var client = new RestClient(conn);
+            var request = new RestRequest();
+            var response = client.Execute(request);
+            listView.Scrollable = true;
+            listView.View = View.Details;
+            ColumnHeader header = new ColumnHeader();
+            header.Text = "Questions";
+            header.Name = "list";
+            header.Width = listView.Width;
+            listView.Columns.Add(header);
+            listView.Items.Clear();
+
+            var result = JsonConvert.DeserializeObject<List<Questions>>(response.Content);
+            foreach(var item in result)
+            {
+                listView.Items.Add(item.question);
+            }
+            
+            listView.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+        public void LoadListAnswer(ListView listView)
+        {
+            string conn = "https://localhost:7045/api/Questions";
+            var client = new RestClient(conn);
+            var request = new RestRequest();
+            var response = client.Execute(request);
+            listView.Scrollable = true;
+            listView.View = View.Details;
+            ColumnHeader header = new ColumnHeader();
+            header.Text = "Answer";
+            header.Name = "list";
+            header.Width = listView.Width;
+            listView.Columns.Add(header);
+            listView.Items.Clear();
+
+            var result = JsonConvert.DeserializeObject<List<Questions>>(response.Content);
+            foreach (var item in result)
+            {
+                listView.Items.Add(item.answer);
+            }
+
+            listView.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            RestClient client = new RestClient("https://localhost:7045/api/Questions");
+            Questions questions = new Questions();
+            var request = new RestRequest("api/Questions").AddJsonBody(questions);
+            var response = client.ExecutePostAsync<Questions>(request);
+        }
+
+        private void btnSend_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button thisButton = (Button)sender;
+            thisButton.BackColor = ThemeColor.SecondaryColor;
+        }
+
+        private void btnSend_MouseUp(object sender, MouseEventArgs e)
+        {
+            Button thisButton = (Button)sender;
+            thisButton.BackColor = ThemeColor.PrimaryColor;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Questions
+        {
+            public int id { get; set; }
+            public string question { get; set; }
+            public string answer { get; set; }
+            public string name { get; set; }
+            public Questions()
+            {
+
+            }
+            public Questions(int id, string question, string answer, string name)
+            {
+                this.id = id;
+                this.question = question;
+                this.answer = answer;
+                this.name = name;
+            }
         }
     }
 }
